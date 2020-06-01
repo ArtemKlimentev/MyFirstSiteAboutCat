d3a7a962fc1bf7d280ce8e83f3ee3db218c64019
ArtemKlimentev <65297142+ArtemKlimentev@users.noreply.github.com>
2020-06-01 10:55:39 +0300

    Добавил попап

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.getElementById("phot").src="img/banner-2.png"
 root.onclick=function(){
 document.getElementById("phot").src="img/banner-1.jpg.png"
 }
+var popap=document.querySelector(".kont-popap");
+var popapClose=document.querySelector(".close");
+var form=document.querySelector(".popap");
+popap.addEventListener("click",function(evt){
+  evt.preventDefault();
+  form.classList.add("vis");
+});
+popapClose.addEventListener("click",function(evt){
+    evt.preventDefault();
+    form.classList.remove("vis");
+});
