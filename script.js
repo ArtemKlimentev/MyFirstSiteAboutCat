954e6efdb5c902b7e02a94579fc0639dbe71b6bf
ArtemKlimentev <ArtemKlimentev@users.noreply.github.com>
2020-06-17 17:58:55 +0300

    ПРОПАЖА

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 change.onclick=function(){
 document.getElementById("phot").src="img/banner-2.png"
 }
@@ -9,6 +14,7 @@ var popapClose=document.querySelector(".close");
 var form=document.querySelector(".popap");
 var yes=document.querySelector(".yes");
 var popapSub = document.querySelector('.popap-button')
+var popapSub2 = document.querySelector('.popap-button2')
 popap.addEventListener("click",function(evt){
   evt.preventDefault();
   form.classList.add("popap-show");
@@ -21,4 +27,9 @@ popapSub.addEventListener('click',function(evt){
 	evt.preventDefault();
     form.classList.remove("popap-show");
     yes.classList.add('yes-show');	
-})
+    
+});
+popapSub2.addEventListener('click',function(evt){
+	evt.preventDefault();	
+yes.classList.remove('yes-show')
+});
